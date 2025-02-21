bd8b7e419b314e266c3218280fa512158b84f168
TheMickQc <mickael.marcotte2004@gmail.com>
2025-02-21 11:54:18 -0500

    some changes

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,15 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local config = require("nvim-treesitter.configs")
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local config = require("nvim-treesitter.configs")
 
-        config.setup({
-            ensure_installed = {"lua", "c", "javascript"},
-            highlight = { enable = true },
-            indent = { enable = true }
-        })
-    end
+            config.setup({
+                ensure_installed = {"lua", "c", "javascript"},
+                highlight = { enable = true },
+                indent = { enable = true }
+            })
+        end
+    }
 }
